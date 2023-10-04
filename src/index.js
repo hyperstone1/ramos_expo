d08481f7c424a72d41b74e4dd9ee1e5fd45b5fe8
hyperstone1 <kingofthegorgon@mail.ru>
2023-10-04 14:43:17 +0300

    up project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import './js/components/header';
+import './js/components/footer';
+import './js/pages/home';
+// import './js/utils/map';
+import './index.scss';
