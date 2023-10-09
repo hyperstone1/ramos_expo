1116f6ed34a7195a114b0027a9c1c8bff597bc37
Kie6er <kirill.zawgorodny@yandex.ru>
2023-10-09 16:45:09 +0300

    add form validation

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -15,29 +15,10 @@ $(document).ready(function () {
 	$('.modal-back').on('click', closeModal);
 	$('.modal-exit').on('click', closeModal);
 
-	$('.form').on('submit', (evt) => {
-		evt.preventDefault();
-		$(this).find('input').val('');
-		$(this).find('textarea').val('');
-
-		if ($('.modal').hasClass('active')) {
-			switchModalContent()
-		} else {
-			$('.modal').addClass('active');
-			$('body').addClass('lock');
-			switchModalContent()
-		}
-	})
-
 	function closeModal() {
 		$('.modal').removeClass('active');
 		$('body').removeClass('lock');
 		$('.modal').find('.modal-content--first').addClass('active');
 		$('.modal').find('.modal-content--second').removeClass('active');
-	}
-
-	function switchModalContent() {
-		$('.modal').find('.modal-content--first').removeClass('active');
-		$('.modal').find('.modal-content--second').addClass('active');
 	}
 })
